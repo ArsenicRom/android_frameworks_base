2f70aab70e947f812222bba3cd44530e737ce0de
eyosen <abittin@gmail.com>
2015-12-20 22:08:59 +0200

    QS: add Screenrecord tile

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -56,6 +56,7 @@ import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.ReadingModeTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.CalcTile;
 import com.android.systemui.qs.tiles.SmartPixelsTile;
@@ -175,6 +176,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new SleepScreenTile(mHost);
             case "suspend_action":
                 return new SuspendActionTile(mHost);
+            case "screenrecord":
+                return new ScreenrecordTile(mHost);
         }
 
         // Intent tiles.
