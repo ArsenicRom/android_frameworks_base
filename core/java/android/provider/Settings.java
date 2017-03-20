5db67842e9e292148f3a0aa5fab62137c29c3b2f
ArsenicRom <tumayordeseo5@hotmail.com>
2017-03-20 11:49:31 -0400

    Lockscreen battery info[2/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3887,6 +3887,18 @@ public final class Settings {
          */
         public static final String QS_SHOW_BRIGHTNESS_ICON = "qs_show_brightness_icon";
 
+        /**
+         * Whether to show the negociated charger current in the lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_CHARGING_CURRENT = "lockscreen_charging_current";
+
+        /**
+         * Whether to show the battery info on the lockscreen while charging
+         * @hide
+         */
+        public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
