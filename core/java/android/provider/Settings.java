aa181303f683b3cbc8e4279118af9186ff6896b9
Varun Date <date.varun123@gmail.com>
2017-08-10 10:19:54 +0530

    FWB:Single tap tile add switch [1/2]
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3984,6 +3984,12 @@ public final class Settings {
         public static final String SENSOR_BLOCK = "sensor_block";
 
         /**
+         *
+         * @hide
+         */
+        public static final String QUICK_TILE_ADD = "quick_tile_add";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -10375,3 +10381,4 @@ public final class Settings {
         return packages[0];
     }
 }
+
