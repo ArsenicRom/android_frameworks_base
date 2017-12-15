1484a06362503595e2183806dd12a61bebde7487
maxwen <max.weninger@gmail.com>
2017-12-15 19:55:46 +0100

    Configure SystemUI theme [1/2]
    In 8.1 several elements change depending on the dominant color of the homescreen
    wallpaper. If is a light/white wallpaper these elements will turn white and if is
    a dark/black wallpaper then these elements will turn black. If is a neutral color
    then it defaults to the light/white color.
    
    This commits makes all of this configurable and so places the option in the user's
    hands. They can either leave it up to automatic selection (wallpaper api) or force
    a light or dark theme.
    
    -- Commit message was rewritten to better explain what's up :-)
    
    Original commit message:
    
    allow to force light or dark or use the wallpaper based
    automatic selection
    
    Change-Id: Id7c287ea82cf76e6d3189c5621258cefe2c4d6d2

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4168,10 +4168,24 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         // 0 = auto, 1 = time-based, 2 = light, 3 = dark
         final int globalStyleSetting = LineageSettings.System.getInt(mContext.getContentResolver(),
                 LineageSettings.System.BERRY_GLOBAL_STYLE, 0);
-        WallpaperColors systemColors = mColorExtractor
-                .getWallpaperColors(WallpaperManager.FLAG_SYSTEM);
-        final boolean wallpaperWantsDarkTheme = systemColors != null
-                && (systemColors.getColorHints() & WallpaperColors.HINT_SUPPORTS_DARK_THEME) != 0;
+        int userThemeSetting = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.SYSTEM_UI_THEME, 0, mCurrentUserId);
+        boolean useDarkTheme = false;
+        if (userThemeSetting == 0) {
+            // The system wallpaper defines if QS should be light or dark.
+            WallpaperColors systemColors = mColorExtractor
+                    .getWallpaperColors(WallpaperManager.FLAG_SYSTEM);
+            useDarkTheme = systemColors != null
+                    && (systemColors.getColorHints() & WallpaperColors.HINT_SUPPORTS_DARK_THEME) != 0;
+            // Check for black and white accent so we don't end up
+            // with white on white or black on black
+            unfuckBlackWhiteAccent();
+        } else {
+            useDarkTheme = userThemeSetting == 2;
+            // Check for black and white accent so we don't end up
+            // with white on white or black on black
+            unfuckBlackWhiteAccent();
+        }
         final Configuration config = mContext.getResources().getConfiguration();
         final boolean nightModeWantsDarkTheme = DARK_THEME_IN_NIGHT_MODE
                 && (config.uiMode & Configuration.UI_MODE_NIGHT_MASK)
@@ -4192,10 +4206,6 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                 useDarkTheme = wallpaperWantsDarkTheme || nightModeWantsDarkTheme;
                 break;
         }
-
-            // Check for black and white accent so we don't end up
-            // with white on white or black on black
-            unfuckBlackWhiteAccent();
 
         if (isUsingDarkTheme() != useDarkTheme) {
             mUiOffloadThread.submit(() -> {
@@ -5129,6 +5139,9 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.ACCENT_PICKER),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SYSTEM_UI_THEME),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -5151,6 +5164,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
          public void update() {
             setQsPanelOptions();
             setQsRowsColumns();
+            updateTheme();
         }
     }
 
