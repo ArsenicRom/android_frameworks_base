86d840a0f3b26b9f9140f99ec356224a3daafffc
arsenicrom <tumayordeseo5@hotmail.com>
2018-05-14 23:20:44 -0400

    Revert "Themes: build as apps"
    This reverts commit 433694086557afcae3346ccce8e48635f3cacd4b.

--- packages/overlays/DuiBlackThemeOverlay/Android.mk
+++ packages/overlays/DuiBlackThemeOverlay/Android.mk
@@ -1,8 +1,14 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := optional
-LOCAL_PACKAGE_NAME := DuiBlackThemeOverlay
-LOCAL_SDK_VERSION := current
+
 LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := false
-include $(BUILD_PACKAGE)
+
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+
+LOCAL_RRO_THEME := DuiBlackTheme
+
+LOCAL_PACKAGE_NAME := DuiBlackThemeOverlay
+
+include $(BUILD_RRO_PACKAGE)
--- packages/overlays/DuiDarkThemeOverlay/Android.mk
+++ packages/overlays/DuiDarkThemeOverlay/Android.mk
@@ -1,8 +1,14 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := optional
-LOCAL_PACKAGE_NAME := DuiDarkThemeOverlay
-LOCAL_SDK_VERSION := current
+
 LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := false
-include $(BUILD_PACKAGE)
+
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+
+LOCAL_RRO_THEME := DuiDarkTheme
+
+LOCAL_PACKAGE_NAME := DuiDarkThemeOverlay
+
+include $(BUILD_RRO_PACKAGE)
